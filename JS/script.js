cd574bbb105ec7a0330e4f1c37949c9b2678d8a7
Muriel Godoi <murielgodoi@gmail.com>
2023-08-22 17:03:43 -0300

    Add files to main branch

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+function redirectToCalcC() {
+    // Redirecionar para a página de cálculo do Coeficiente C
+}
+
+function redirectToConcentrationTime() {
+    // Redirecionar para a página de cálculo do Tempo de Concentração
+}
+
+function redirectToLearnMore() {
+    // Redirecionar para a página de informações adicionais
+}
